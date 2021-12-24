77b33b1bb2cd7b6b9227c0672fd16cd3b7c46935
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-24 12:22:46 +0600

    Added new confitm alert prompt

---
+++ script.js
@@ -0,0 +1,26 @@
+// var itGirls = true;
+// if (itGirls = female) {
+//     alert("You can participate in the project!");
+// }
+// else {
+//     alert("You can not participate in the project!")
+// }
+
+
+// var username = prompt("What is your name?");
+// alert("Welcome" + username);
+
+// var project = 20;
+// if (project >= 16) {
+//     alert("You can participate in the project!");
+// }
+// else {
+//     alert("You can not participate in the project!");
+// }
+// if(confirm("Are you children of migration?")) {
+//     alert("Welcome, Do you like read the books!");
+// }
+// else {
+//     alert("You can not participate in this project!")
+// }
+ 
